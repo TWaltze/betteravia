6d50790a0c6ac0dcee1dd20a87b3a77b36bbe2a7
Tyler Waltze <tylerwaltze@gmail.com>
2015-06-27 23:53:32 -0400

    Load map objects from an array
    Represent a map via an array of integers.

--- public/js/Models/Map.js
+++ public/js/Models/Map.js
@@ -1,10 +1,47 @@
 Map = function(game) {
     this.game = game;
     this.land = null;
-    this.width = 2048 * .5;
-    this.height = 1024 * .5;
+    this.width = 2048;
+    this.height = 1024;
+
+    this.water = [];
 
     this.thing = null;
+
+    this.tileSize = 35;
+    this.mapSize = 30;
+    this.level = [
+        [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0],
+        [0,1,0,4,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,1,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,2,0,0,1,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,9,9,9,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,9,9,0,0,0,0],
+        [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,3,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,6,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0],
+        [0,0,2,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,1,0,0,3,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,1,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,1,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,2,0,0,0,3,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,1,0,0,2,0,0,7,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0],
+        [0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0]
+    ];
 }
 
 Map.prototype = {
@@ -32,16 +69,11 @@ Map.prototype = {
         this.game.load.image('mine', 'public/images/tiles/mine.png');
 
         this.game.load.spritesheet('characterAnim', 'public/images/tiles/characterAnim.png', 70, 74);
+
+        this.game.load.atlasJSONHash('tileset', 'public/assets/tileset.png', 'public/assets/tileset.json');
     },
 
     create: function() {
-        // Resize our game world to be a 2000 x 2000 square
-        // this.game.world.setBounds(-500, -500, 1000, 1000);
-
-        // Our tiled scrolling background
-        // this.land = game.add.tileSprite(0, 0, 800, 600, 'earth');
-        // this.land.fixedToCamera = true;
-
         // set the Background color of our game
     	this.game.stage.backgroundColor = "0xde6712";
 
@@ -55,18 +87,17 @@ Map.prototype = {
 
         // create the floor tiles
         var floorTile;
-        for (var xt = this.height; xt > 0; xt -= 35) {
-            for (var yt = this.height; yt > 0; yt -= 35) {
-            	floorTile = this.game.add.isoSprite(xt, yt, 0, 'tile', 0, this.floorGroup);
-            	floorTile.anchor.set(0.5);
-
+        for (var xt = 0; xt < this.mapSize * this.tileSize; xt += this.tileSize) {
+            for (var yt = 0; yt < this.mapSize * this.tileSize; yt += this.tileSize) {
+                floorTile = this.game.add.isoSprite(xt, yt, 0, 'tile', 0, this.floorGroup);
+                floorTile.anchor.set(0.5);
             }
         }
 
         // create the grass tiles randomly
         var grassTile;
-        for (var xt = this.height; xt > 0; xt -= 35) {
-            for (var yt = this.height; yt > 0; yt -= 35) {
+        for (var xt = 0; xt < this.mapSize * this.tileSize; xt += this.tileSize) {
+	        for (var yt = 0; yt < this.mapSize * this.tileSize; yt += this.tileSize) {
 
                 var rnd = rndNum(20);
 
@@ -87,7 +118,39 @@ Map.prototype = {
             }
         }
 
-        // create an immovable cactus tile and randomly choose one of two graphical cactus representations
+        var obstacle;
+        for (var yt = 0; yt < this.level.length; yt++) {
+            var tile = this.level[yt];
+
+            for (var xt = 0; xt < this.level[yt].length; xt++) {
+                var obstacleType = null;
+
+                if (tile[xt] == 1) {
+                    obstacleType = 'cactus1';
+                } else if (tile[xt] == 2) {
+                    obstacleType = 'cactus2';
+                } else if (tile[xt] == 3) {
+                    obstacleType = 'rock'
+                }
+
+                if (obstacleType) {
+                    obstacle = this.game.add.isoSprite(xt * this.tileSize, yt * this.tileSize, 0, obstacleType, 0, this.obstacleGroup);
+
+                    obstacle.anchor.set(0.5);
+
+                    // Let the physics engine do its job on this tile type
+                    this.game.physics.isoArcade.enable(obstacle);
+
+                    // This will prevent our physic bodies from going out of the screen
+                    obstacle.body.collideWorldBounds = true;
+
+                    // Make the obstacle body immovable
+                    obstacle.body.immovable = true;
+                }
+            }
+        }
+        //
+        // // create an immovable cactus tile and randomly choose one of two graphical cactus representations
         // var cactus1;
         // for (var xt = this.height; xt > 0; xt -= 400) {
         //     for (var yt = this.height; yt > 0; yt -= 400) {
@@ -153,11 +216,29 @@ Map.prototype = {
 
         // set the slow down rate on each axis (X, Y, Z)
         // this.thing.body.drag.set(100, 100, 0);
+
+        // create the floor tiles
+        // this.floorGroup = this.game.add.group();
+        //
+        // var floorTile;
+        // var size = 32;
+        // for (var y = size; y <= game.physics.isoArcade.bounds.frontY; y += size) {
+        //     for (var x = size; x <= game.physics.isoArcade.bounds.frontX; x += size) {
+        //     	floorTile = this.game.add.isoSprite(x, y, 0, 'tileset', 'grass', this.floorGroup);
+        //     	floorTile.anchor.set(0.5, 1);
+        //         floorTile.smoothed = false;
+        //     }
+        // }
     },
 
     update: function() {
         this.game.physics.isoArcade.collide(this.obstacleGroup);
         this.game.iso.topologicalSort(this.obstacleGroup);
+
+        // this.water.forEach(function (w) {
+        //     w.isoZ = (-2 * Math.sin((this.game.time.now + (w.isoX * 7)) * 0.004)) + (-1 * Math.sin((this.game.time.now + (w.isoY * 8)) * 0.005));
+        //     w.alpha = Phaser.Math.clamp(1 + (w.isoZ * 0.1), 0.2, 1);
+        // });
     }
 }
 
